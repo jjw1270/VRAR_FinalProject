c3370300786f4bab07d98e74ab06bd40dc044416
Yunjae Jang <jjw1270@naver.com>
2022-05-13 23:45:20 +0900

    update

--- VRAR_Project/Assets/#Scripts/Main/PlayerCtrl.cs
+++ VRAR_Project/Assets/#Scripts/Main/PlayerCtrl.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class PlayerCtrl : MonoBehaviour
+public class PlayerCtrl : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public Image aim;
+    private Color aimColor;
+    private PhotonView pv;
     void Start()
     {
+        if(!pv.IsMine) return;
+
+        this.gameObject.tag = "Mine";
+        aimColor = aim.color;
+    }
+
+    void Update()
+    {
+        if(!pv.IsMine) return;
+        rayCtrl();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void rayCtrl(){
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1000;
+
+        if(Physics.Raycast(transform.position, forward, out hit)){
+            if(hit.transform.gameObject.layer == LayerMask.NameToLayer("CanHit")){   //레이어 비교
+                aim.color = Color.red;
+                if(hit.transform.CompareTag("Enemy")){
+                    fire(hit.transform);
+                }
+                else if(hit.transform.CompareTag("블라블라")){
+                    //블라블라
+                }
+                
+            }
+            else{
+            aim.color = aimColor;
+            }
+        }
     }
+
+    void fire(Transform target){
+        
+        float time = 0;
+        time += Time.deltaTime;
+
+
+    }
+
 }
--- VRAR_Project/Assets/#Scripts/Main/PlayerMove.cs
+++ VRAR_Project/Assets/#Scripts/Main/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PlayerMove : MonoBehaviourPunCallbacks
+public class PlayerMove : MonoBehaviourPun
 {
     PhotonView pv;
     public GameObject player;
@@ -16,16 +16,19 @@ public class PlayerMove : MonoBehaviourPunCallbacks
     void Start()
     {
         pv = this.GetComponent<PhotonView>();
+        if(!pv.IsMine)
+            mainCam.GetComponent<Camera>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //if(!pv.IsMine) return;
-        moveSpeed += 0.05f * Time.deltaTime;
+        moveSpeed += 0.2f * Time.deltaTime;
         if(moveSpeed >= 0.8f)
             moveSpeed = 0.8f;
         MoveLookAt();
+
     }
 
     void MoveLookAt(){
@@ -37,15 +40,26 @@ public class PlayerMove : MonoBehaviourPunCallbacks
 
         if(delta_angle < 0){
             player.transform.rotation = Quaternion.Lerp(player.transform.rotation, 
-                Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, 45), Time.deltaTime);
+                Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, 70), Time.deltaTime);
         }
         else if(delta_angle > 0){
             player.transform.rotation = Quaternion.Lerp(player.transform.rotation, 
-                Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, -45), Time.deltaTime);
+                Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, -70), Time.deltaTime);
         }
         else {
             player.transform.rotation = Quaternion.Lerp(player.transform.rotation, 
                 Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, 0), Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.transform.CompareTag("Boundary")){
+            //맵의 경계에 부딪히면 10초안에 돌아가라고 경고, 10초 지나면 사망
+            Debug.Log("Boundary");
+        }
+        else if(other.transform.CompareTag("MapColli")){
+            //지형과 부딪히면 폭파됨
+            Debug.Log("MAP");
         }
     }
 }
